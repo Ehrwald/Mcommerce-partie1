0a115f4bb6ac1825e5600d7c1a9ccb8a039a62d4
ehrwald <franck.hubschwerle@uha.fr>
2019-07-08 22:45:00 +0200

    partie3

--- src/main/java/com/ecommerce/microcommerce/web/exceptions/ProduitGratuitException.java
+++ src/main/java/com/ecommerce/microcommerce/web/exceptions/ProduitGratuitException.java
@@ -3,6 +3,7 @@ package com.ecommerce.microcommerce.web.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+// je l'ai fait dans la partie 1 désolé.
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ProduitGratuitException extends Throwable {
 
